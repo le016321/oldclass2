dcc6512deff10a59fdb5cedc496caaf4404e5fc0
Lizzy E <le016321@ohio.edu>
2023-01-24 12:10:55 -0500

    @yhzhang1

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Lizzy Ehleringer
+ *        Date: 1/24/2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
